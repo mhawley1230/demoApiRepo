a53e37bcea9713dc418f0ac65458ddd8b07716d1
Nathan <nate01776@gmail.com>
2017-05-23 11:02:50 -0400

    DB confirmed and tested, /games endpoint implemented with basic login

--- app.rb
+++ app.rb
@@ -1,7 +1,46 @@
 require 'sinatra'
-require 'sinatra/ActiveRecord'
+require 'pry'
+require 'sinatra/activerecord'
 require './config/environments'
+require './models/game'
 
 get '/' do
   'Hello, World!'
+end
+
+get '/games' do
+  @all_games = Game.all
+  if params['week'] == nil then
+    return {
+      status: '400',
+      message: 'Please provide week parameter in query',
+      example: '/games?week=4 OR /games?week=4,5,6'
+    }.to_json
+  else
+    @query_week = params['week'].split(',')
+  end
+
+  output_array = []
+  @all_games.each do |game|
+    @query_week.each do |week|
+      if game.week == week
+        output_array << {
+          id: game.id,
+          home: game.home,
+          away: game.away,
+          week: game.week,
+          gamecode: game.gamecode,
+          _link: "/games/#{game.id}"
+        }
+      end
+    end
+  end
+
+  output_object = {
+    status: 200,
+    results_count: output_array.length,
+    results: output_array
+  }
+
+  return output_object.to_json
 end
