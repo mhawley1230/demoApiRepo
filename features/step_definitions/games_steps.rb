6a82a6fcb3ed02ad6df59e1149d7aeec9296a7e5
Nathan <nate01776@gmail.com>
2017-08-21 15:03:25 -0400

    wrapping up

--- features/step_definitions/games_steps.rb
+++ features/step_definitions/games_steps.rb
@@ -2,6 +2,7 @@ Given("I navigate to games and I don't supply a parameter") do
 end
 
 When("I run the RAPI testcase 'no parameters'") do
+  system('C:/Users')
   # %x(cd C:/Program Files/SmartBear/ReadyAPI-2.1.0/bin)
   # system("C:\\Program Files\\SmartBear\\ReadyAPI-2.1.0\\bin\\testrunner.bat -sgames '-cNo Parameters' -r '-RTestCase Report' '-EDefault environment' C:\\Users\\Nathan.Wright\\codeRepo\\demoApiRepo\\features\\nflAPIproject-readyapi-project")
   # system("C:\\Program Files\\SmartBear\\ReadyAPI-2.1.0\\bin\\testrunner.bat -sgames '-cNo Parameters' -r -a -fC:\\Users\\Nathan.Wright\\codeRepo\\demoApiRepo\\features\\nflAPIproject-readyapi-project\\cucumberReports '-RTestCase Report' '-EDefault environment' C:\\Users\\Nathan.Wright\\codeRepo\\demoApiRepo\\features\\nflAPIproject-readyapi-project")
