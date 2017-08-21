de3352bc8b094653017a8079304a20ec9658a2e3
Nathan <nate01776@gmail.com>
2017-08-21 14:26:00 -0400

    working on project

---
+++ features/step_definitions/games_steps.rb
@@ -0,0 +1,13 @@
+Given("I navigate to games and I don't supply a parameter") do
+end
+
+When("I run the RAPI testcase 'no parameters'") do
+  # %x(cd C:/Program Files/SmartBear/ReadyAPI-2.1.0/bin)
+  # system("C:\\Program Files\\SmartBear\\ReadyAPI-2.1.0\\bin\\testrunner.bat -sgames '-cNo Parameters' -r '-RTestCase Report' '-EDefault environment' C:\\Users\\Nathan.Wright\\codeRepo\\demoApiRepo\\features\\nflAPIproject-readyapi-project")
+  # system("C:\\Program Files\\SmartBear\\ReadyAPI-2.1.0\\bin\\testrunner.bat -sgames '-cNo Parameters' -r -a -fC:\\Users\\Nathan.Wright\\codeRepo\\demoApiRepo\\features\\nflAPIproject-readyapi-project\\cucumberReports '-RTestCase Report' '-EDefault environment' C:\\Users\\Nathan.Wright\\codeRepo\\demoApiRepo\\features\\nflAPIproject-readyapi-project")
+  # exec( "echo 'hi'" )
+  # exec( "C:\\Program Files\\SmartBear\\ReadyAPI-2.1.0\\bin\\testrunner.bat -s\\games '-cNo Parameters' '-RTestCase Report' '-EDefault environment' C:\\Users\\Nathan.Wright\\codeRepo\\demoApiRepo\\features\\nflAPIproject-readyapi-project" )
+end
+
+Then("I should get a 400 status") do
+end
